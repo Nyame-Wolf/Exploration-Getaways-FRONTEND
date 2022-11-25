dfced44235ea9730adfe085a1c6549bf021a55f6
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-25 17:52:42 -0300

    Setup index and app js files
    Create assets, components, pages and redux folders
    
    Implement Routes and Route on app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
+// import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Routes>
+      <Route path="/" element={<HomePage />} />
+        <Route path="/package/details" element={<DetailsPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/reservations" element={<ReservationsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Routes> */}
     </div>
   );
 }
