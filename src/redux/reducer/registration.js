f4f099ccfe25dfc58ab1b3a96984b8a14cd436c7
Nyame-Wolf <mumenyam@gmail.com>
2022-11-30 23:49:03 +0300

    add signin reducer
    add asignin page and component

--- src/redux/reducer/registration.js
+++ src/redux/reducer/registration.js
@@ -13,10 +13,33 @@ export const postSignUp = createAsyncThunk(
       body: JSON.stringify({ user }),
     }).then((res) => {
       if (res.ok) {
+        console.log(res.headers.get('Authorization'));
         localStorage.setItem('token', res.headers.get('Authorization'));
         return res.json();
       }
       throw new Error(res);
+    })
+      .then((json) => console.dir(json))
+      .catch((err) => console.error(err));
+  },
+);
+
+export const postSignIn = createAsyncThunk(
+  'users/signin',
+  async (user) => {
+    await fetch('http://127.0.0.1:4000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user }),
+    }).then((res) => {
+      if (res.ok) {
+        console.log(res.headers.get('Authorization'));
+        localStorage.setItem('token', res.headers.get('Authorization'));
+        return res.json();
+      }
+      return res.text().then((text) => Promise.reject(text));
     })
       .then((json) => console.dir(json))
       .catch((err) => console.error(err));
