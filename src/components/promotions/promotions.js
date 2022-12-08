d29808e67f5422ddb301812d2b8bbcac84bf6aae
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-08 13:33:22 -0300

    Add fix changes

--- src/components/promotions/promotions.js
+++ src/components/promotions/promotions.js
@@ -7,76 +7,81 @@ import { Link } from 'react-router-dom';
 import loadingGif from '../../assets/images/loading.gif';
 
 function Promotions() {
-  const packages = useSelector((state) => state.agency.data);
+  const promotions = useSelector((state) => state.agency.data);
   const status = useSelector((state) => state.agency.status);
 
   return (
     <div className="home-container">
-      {status === 'succeeded' ? packages.map((item) => (
-        <Link state={item} key={item.id} to="package/details">
-          <div key={item.id} className="package">
-            <div className="package-image-div">
-              <img src={item.photo} className="package-image" alt="package-view" />
-              <div className="promotion">
-                <span>
-                  -
-                  {item.promotion}
-                  %
-                </span>
-              </div>
-              {item.flight
-                ? (
-                  <div className="flight-icon">
-                    <span>Flight included</span>
-                    <FaPlaneDeparture />
-                  </div>
-                )
-                : ''}
-            </div>
-            <div className="package-information">
-              <div className="information-texts">
-                <p className="title">{item.title}</p>
-                <div className="included-div">
-                  <div className="included">
-                    <FaGlobeAmericas />
+      {status === 'succeeded' ? promotions.map((item) => {
+        if (item.promotion) {
+          return (
+            <Link state={item} key={item.id} to="package/details">
+              <div key={item.id} className="package">
+                <div className="package-image-div">
+                  <img src={item.photo} className="package-image" alt="package-view" />
+                  <div className="promotion">
                     <span>
-                      {' '}
-                      {item.destination}
+                      -
+                      {item.promotion}
+                      %
                     </span>
                   </div>
-                  <div className="included">
-                    <FaRegCalendarAlt />
+                  {item.flight
+                    ? (
+                      <div className="flight-icon">
+                        <span>Flight included</span>
+                        <FaPlaneDeparture />
+                      </div>
+                    )
+                    : ''}
+                </div>
+                <div className="package-information">
+                  <div className="information-texts">
+                    <p className="title">{item.title}</p>
+                    <div className="included-div">
+                      <div className="included">
+                        <FaGlobeAmericas />
+                        <span>
+                          {' '}
+                          {item.destination}
+                        </span>
+                      </div>
+                      <div className="included">
+                        <FaRegCalendarAlt />
+                        <span>
+                          {' '}
+                          {item.hotel}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="information-price">
+                    <span>
+                      from
+                      {' '}
+                    </span>
+                    <span className="line-strike">
+                      $
+                      {item.price}
+                    </span>
                     <span>
                       {' '}
-                      {item.hotel}
+                      for
+                      {' '}
                     </span>
+                    <p className="price">
+                      $
+                      {(item.price * ((100 - item.promotion) / 100)).toFixed(0)}
+                    </p>
+                    <span>12x payment</span>
                   </div>
                 </div>
               </div>
-              <div className="information-price">
-                <span>
-                  from
-                  {' '}
-                </span>
-                <span className="line-strike">
-                  $
-                  {item.price}
-                </span>
-                <span>
-                  {' '}
-                  for
-                  {' '}
-                </span>
-                <p className="price">
-                  $
-                  {(item.price * ((100 - item.promotion) / 100)).toFixed(0)}
-                </p>
-                <span>12x payment</span>
-              </div>
-            </div>
-          </div>
-        </Link>
-      )) : <img className="loading-gif" src={loadingGif} alt="loading" /> }
+            </Link>
+          );
+        }
+        return null;
+      }) : <img className="loading-gif" src={loadingGif} alt="loading" /> }
     </div>
   );
 }
